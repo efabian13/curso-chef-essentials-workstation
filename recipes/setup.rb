29190af89524d8363fa8eae9a212418047ae0952
Este <e@e.es>
2017-05-11 18:56:42 +0000

    /etc/motd ESTATICO

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,5 +2,15 @@ package 'tree' do
 	action :install
 end
 file '/etc/motd' do
-	content "Property of...\n"
+	content "Property of...\n
+
+  IPADDRESS: 104.236.192.102
+  HOSTNAME : banana-stand
+  MEMORY   : 502272 kB
+  CPU      : 2399.98 MHz
+"
+
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
